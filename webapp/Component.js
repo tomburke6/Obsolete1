aba10f44f5af8f6284557d57d0821b4e891adbf4
Thomas Burke <thomas.burke@itelligencegroup.com>
2020-12-28 02:09:31 +0000

    Initial Commit to Master Branch

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/generic/app/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("fiori.display.purchase.documents.PurchaseDocumentsALP.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
